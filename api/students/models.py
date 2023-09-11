032088a89ad619d7528e451a969cdc6866afecd2
neochrist <kikouchia58@gmail.com>
2023-09-11 09:41:25 +0400

    added models

--- api/students/models.py
+++ api/students/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name: str = models.CharField(max_length=50)
+    last_name: str = models.CharField(max_length=50)
+    phone_number: str = models.CharField(max_length=15)
+    
+    #to add subjects__involved
+
+class Subject(models.Model):
+    subject_name: str = models.CharField(max_length=50)
+    proffessor_name: str = models.CharField(max_length=50)
+
+
